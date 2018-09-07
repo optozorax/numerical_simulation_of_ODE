d9a6c98530c921eac550972b1243653b332d481e
David <singerous@gmail.com>
2018-09-07 11:59:05 +0700

    "math.h" lib added
    1. "math.h" lib added

--- 1/method1.cpp
+++ 1/method1.cpp
@@ -1,6 +1,25 @@
 #include "methods.h"
 
 std::vector<double> solveDE_Euler_Explicit(double a, double b, double h, double ya, DE_F f) {
-	int n = (b-a)/h;
-	return std::vector<double>(n, 0);
+    int n = (b-a)/h;
+
+    std::vector<double> result(n+1, 0);
+    result[0] = ya;
+
+    double t_n = a;
+
+    for (int i = 1; i < n+1; ++i) {
+
+        t_n += h;
+
+        double& y_n1 = result[i];
+        double& y_n = result[i-1];
+
+        y_n1 = y_n + h*f(t_n, y_n);
+
+    }
+
+    result.erase(result.begin());
+
+    return result;
 }
