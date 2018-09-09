f529fc2d3b6cf3e515ccd495dce9284e7ded3cb9
Sl07h <Sl07h@github.com>
2018-09-09 21:58:33 +0700

    3rd method + output fix
    Implemented 3rd method and fixed output of tables in the 1st task.

--- 1/methods.h
+++ 1/methods.h
@@ -2,6 +2,9 @@
 
 #include <vector>
 #include <functional>
+#include <iomanip>
+const int _PRECISION_ = 5;
+
 
 typedef std::function<double(double, double)> DE_F;
 typedef std::function<double(double)> DE_Solve;
